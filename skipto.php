7e91e13b1f99a1e3f32d76599d9fbb0336aa9d46
schulle4u <schulle3o@yahoo.de>
2016-07-27 21:27:31 +0200

    Configuration added, needs fix

--- skipto.php
+++ skipto.php
@@ -12,6 +12,16 @@ class YellowSkipto
 	function onLoad($yellow)
 	{
 		$this->yellow = $yellow;
+		$this->yellow->config->setDefault("skiptoHeadings", "h1, h2, h3, h4");
+		$this->yellow->config->setDefault("skiptoMain", "main, [role=main]");
+		$this->yellow->config->setDefault("skiptoLandmarks", "[role=navigation], [role=search]");
+		$this->yellow->config->setDefault("skiptoSections", "nav");
+		$this->yellow->config->setDefault("skiptoIds", "#SkipToA1, #SkipToA2");
+		$this->yellow->config->setDefault("skiptoCustomClass", "");
+		$this->yellow->config->setDefault("skiptoAccesskey", "0");
+		$this->yellow->config->setDefault("skiptoWrap", "false");
+		$this->yellow->config->setDefault("skiptoVisibility", "onfocus");
+		$this->yellow->config->setDefault("skiptoAttachElement", "");
 	}
 	
 	// Handle page extra HTML data
@@ -20,9 +30,28 @@ class YellowSkipto
 		$output = NULL;
 		if($name == "footer")
 		{
+			$output = "<script>
+			var SkipToConfig =
+			{
+				\"settings\": {
+					\"skipTo\": {
+						\"headings\"     : \"".$this->yellow->config->get("skiptoHeadings")."\",
+						\"main\"         : \"".$this->yellow->config->get("skiptoMain")."\",
+						\"landmarks\"    : \"".$this->yellow->config->get("skiptoLandmarks")."\",
+						\"sections\"     : \"".$this->yellow->config->get("skiptoSections")."\",
+						\"ids\"          : \"".$this->yellow->config->get("skiptoIds")."\",
+						\"customClass\"  : \"".$this->yellow->config->get("skiptoCustomClass")."\",
+						\"accesskey\"    : \"".$this->yellow->config->get("skiptoAccesskey")."\",
+						\"wrap\"         : \"".$this->yellow->config->get("skiptoWrap")."\",
+						\"visibility\"   : \"".$this->yellow->config->get("skiptoVisibility")."\",
+						\"attachElement\": \"".$this->yellow->config->get("skiptoAttachElement")."\"
+					}
+				}
+			};
+			</script>\n";
 			$locationJavascript = $this->yellow->config->get("serverBase").$this->yellow->config->get("pluginLocation")."SkipTo.min.js";
 			$fileNameJavascript = $this->yellow->config->get("pluginDir")."SkipTo.min.js";
-			if(is_file($fileNameJavascript)) $output = "<script type=\"text/javascript\" src=\"$locationJavascript\"></script>\n";
+			if(is_file($fileNameJavascript)) $output .= "<script type=\"text/javascript\" src=\"$locationJavascript\"></script>\n";
 		}
 		return $output;
 	}
