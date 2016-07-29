ebf31a77747dc98bcc24ea2181b0f04b2aa9e0fb
schulle4u <schulle3o@yahoo.de>
2016-07-29 11:30:10 +0200

    Modifications by @Markseu: Better JS handling, update.ini added

--- README.md
+++ README.md
@@ -1,29 +1,33 @@
-SkipTo.js plugin 0.6.4
-======================
-Yellow plugin that adds SkipTo-links for keyboard navigation. Uses [SkipTo.js](https://paypal.github.io/skipto/) by PayPal Accessibility Team and University of Illinois. 
+Skipto plugin 0.6.4
+===================
+Yellow plugin that adds keyboard navigation.
 
 How do I install this?
 ----------------------
-1. Download and install [Yellow](https://github.com/datenstrom/yellow/).  
-2. Download [SkipTo Plugin](https://github.com/schulle4u/), and extract it into your `system/plugins` folder.  
+1. [Download and install Yellow](https://github.com/datenstrom/yellow/).
+2. [Download plugin](https://github.com/schulle4u/yellow-plugin-skipto/archive/master.zip). If you are using Safari, right click and select 'Download file as'.
+3. Copy `master.zip` into your `system/plugins` folder.
 
 To uninstall delete the plugin files.
 
-Configuration
--------------
-The following options are available: 
+How to use the keyboard navigation?
+-----------------------------------
+This plugin uses [SkipTo.js v2.0.0](https://paypal.github.io/skipto/) by PayPal Accessibility Team and University of Illinois. It's licensed under BSD.
 
-* skiptoHeadings: which heading elements to show in the menu (h1, h2, h3, h4...)
-* skiptoLandmarks: ARIA landmarks (e.g. banner, navigation, main and search)
-* skiptoSections: HTML5 Section Elements (e.g. main, section[aria-label], section[aria-labelledby]
-* skiptoIds: Any element with the id specified
-* skiptoCustomClass: Any element with the custom class specified
-* skiptoAccesskey: access key to focus the menu, default is 0
-* skiptoWrap (true/false): set the menu to wrap
-* skiptoVisibility (onfocus/onload): set this to "onload" if you want the menu to be always visible 
-
-How to use the SkipTo links
----------------------------
 After loading a page, press the tab key and you will see a `Skip to...` button at the top of the page. Press enter or space to activate a menu containing all headings and sections on the current page. Press the up or down-arrow key to Select one of the items, press enter to directly jump to it. Pressing the accesskey 0 will focus the SkipTo button at any time. 
 
 Please note that this plugin primarily is not designed to enhance accessibility for screen reader users. Modern screen readers provide their own methods to directly skip to headings and other elements. It is made to generally enhance keyboard navigation on large sites, but of course may be useful for screen reader users as well. 
+
+How to configure the keyboard navigation?
+-----------------------------------------
+The following options are available: 
+
+* `skiptoHeadings`: which heading elements to show in the menu (h1, h2, h3, h4...)
+* `skiptoLandmarks`: ARIA landmarks (e.g. banner, navigation, main and search)
+* `skiptoSections`: HTML5 Section Elements (e.g. main, section[aria-label], section[aria-labelledby]
+* `skiptoIds`: Any element with the id specified
+* `skiptoCustomClass`: Any element with the custom class specified
+* `skiptoAccesskey`: access key to focus the menu, default is 0
+* `skiptoWrap`: set the menu to wrap (true/false)
+* `skiptoVisibility`: set this to "onload" if you want the menu to be always visible (onfocus/onload)
+* `skiptoAttachElement`: SkipTo can be attached to any element on the page. If no `attachElement` is found, the script will be attached as the first element after body
--- skipto.php
+++ skipto.php
@@ -2,16 +2,21 @@
 // Copyright (c) 2013-2016 Datenstrom, http://datenstrom.se
 // This file may be used and distributed under the terms of the public license.
 
-// SkipTo.js Plugin
+// Skipto plugin
 class YellowSkipto
 {
-	const Version = "0.6.4";
+	const VERSION = "0.6.4";
 	var $yellow;			//access to API
 	
 	// Handle initialisation
 	function onLoad($yellow)
 	{
 		$this->yellow = $yellow;
+		// Button and menu labels, to be fixed
+		//$this->yellow->config->setDefault("skiptoButtonLabel", "Skip to");
+		//$this->yellow->config->setDefault("skiptoMenuLabel", "Skip To and Page Outline");
+		//$this->yellow->config->setDefault("skiptoLandmarksLabel", "Skip To");
+		//$this->yellow->config->setDefault("skiptoHeadingsLabel", "Page Outline");
 		$this->yellow->config->setDefault("skiptoHeadings", "h1, h2, h3, h4");
 		$this->yellow->config->setDefault("skiptoMain", "main, [role=main]");
 		$this->yellow->config->setDefault("skiptoLandmarks", "[role=navigation], [role=search]");
@@ -21,43 +26,27 @@ class YellowSkipto
 		$this->yellow->config->setDefault("skiptoAccesskey", "0");
 		$this->yellow->config->setDefault("skiptoWrap", "false");
 		$this->yellow->config->setDefault("skiptoVisibility", "onfocus");
-		//$this->yellow->config->setDefault("skiptoAttachElement", "");
+		$this->yellow->config->setDefault("skiptoAttachElement", "document.body");
 	}
 	
 	// Handle page extra HTML data
 	function onExtra($name)
 	{
 		$output = NULL;
-		if($name == "footer")
+		if($name=="footer")
 		{
-			$locationJavascript = $this->yellow->config->get("serverBase").$this->yellow->config->get("pluginLocation")."SkipTo.min.js";
-			$fileNameJavascript = $this->yellow->config->get("pluginDir")."SkipTo.min.js";
-			if(is_file($fileNameJavascript)) 
-			{
-				$output = "<script>
-			var SkipToConfig =
-			{
-				\"settings\": {
-					\"skipTo\": {
-						\"headings\"     : \"".$this->yellow->config->get("skiptoHeadings")."\",
-						\"main\"         : \"".$this->yellow->config->get("skiptoMain")."\",
-						\"landmarks\"    : \"".$this->yellow->config->get("skiptoLandmarks")."\",
-						\"sections\"     : \"".$this->yellow->config->get("skiptoSections")."\",
-						\"ids\"          : \"".$this->yellow->config->get("skiptoIds")."\",
-						\"customClass\"  : \"".$this->yellow->config->get("skiptoCustomClass")."\",
-						\"accesskey\"    : \"".$this->yellow->config->get("skiptoAccesskey")."\",
-						\"wrap\"         : \"".$this->yellow->config->get("skiptoWrap")."\",
-						\"visibility\"   : \"".$this->yellow->config->get("skiptoVisibility")."\",
-					}
-				}
-			};
-			</script>\n
-			<script type=\"text/javascript\" src=\"$locationJavascript\"></script>\n";
-			}
+			$data = array();
+			foreach($this->yellow->config->getData("skipto") as $key=>$value) $data[strtoloweru(substru($key, 6))] = $value;
+			$pluginLocation = $this->yellow->config->get("serverBase").$this->yellow->config->get("pluginLocation");
+			$output .= "<script type=\"text/javascript\">\n";
+			$output .= "var SkipToConfig = { \"settings\": { \"skipTo\": ".json_encode($data)." } };\n";
+			$output .= "var s=document.getElementsByTagName('script')[0], e=document.createElement('script'); e.type='text/javascript'; e.async=true; e.src='{$pluginLocation}SkipTo.min.js';\n";
+			$output .= "s.parentNode.insertBefore(e, s);\n";
+			$output .= "</script>\n";
 		}
 		return $output;
 	}
 }
 
-$yellow->plugins->register("skipto", "YellowSkipto", YellowSkipto::Version);
+$yellow->plugins->register("skipto", "YellowSkipto", YellowSkipto::VERSION);
 ?>
